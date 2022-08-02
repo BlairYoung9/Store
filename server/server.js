762d1ea9f6af6f795c0b792c63b8234f09fe6a8e
Blair Young <blairyoung009@gmail.com>
2022-08-02 16:53:31 -0700

    made DB

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const DB = "inventory_db";
 //Middleware
 app.use(cors(), express.json(), express.urlencoded({extended:true}));
 
+//DB Connection
+require("./config/mongoose.config")(DB)
+
+
 app.listen(PORT, () => {
     console.log(`SERVER IS UP ON: ${PORT}`)
 })
