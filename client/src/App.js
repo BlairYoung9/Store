5ac601fb96e17dc0e9ba1e765778196db547a062
Blair Young <blairyoung009@gmail.com>
2022-08-03 15:23:00 -0700

    index and app.js work

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link, Switch, Route, BrowserRouter} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class = "header">
+        <h1>Blair's Booming Business</h1>
+        <Link to="/">Home</Link> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+        <Link to="/create/new"> Create a Product </Link> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Link to="/blogs"> View All Products </Link>
+        <hr/>
+      </div>
     </div>
   );
 }
