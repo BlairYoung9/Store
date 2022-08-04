4b5e471b4a711258b088ae515550094abc0c8554
Blair Young <blairyoung009@gmail.com>
2022-08-03 17:26:56 -0700

    gwre

---
+++ client/components/All.jsx
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+//display all the products
+const All = (props) => {
+    const [item, setItems] = useState([])
+
+    useEffect( () => {
+        getItemsFromDB()
+    }, [])
+
+    const getItemsFromDB = () => {
+        axios.get("http://localhost:8001/api/items")
+        .then(res => {
+            console.log(res.data);
+            setItems(res.data.items);
+        })
+        .catch (err => console.log(err))
+    }
+    
+    const deleteBlog = (deleteId) => {
+        axios.delete("http://localhost:8001/api/items/" + deleteId)
+            .then( res => {
+                console.log(res.data);
+                console.log("SUCCESS DELETED!");
+
+                // remove from DOM after delete success
+                setItems(items.filter((product) => product._id !== deleteId))
+            })
+            .catch(err => console.log(err))
+    }
+
+    return(
+        <div>
+            <h3> all posts!</h3>
+            {
+                items.map((item,idx) => {
+                    return(
+                        <div key = {item._id}>
+                            <h3><Link to={"/items/" + item._id}>{item.title}</Link></h3>
+                            <button onClick={ () => deleteItem(item._id)}>Delete</button>    
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+};
+export default All;
